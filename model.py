bee77ca4f59f98cd95b3fb687c4a01078d065d06
svenbbs <sbierenbroodspot@outlook.com>
2024-03-21 00:39:57 +0100

    predefined model

--- model.py
+++ model.py
@@ -76,15 +76,17 @@ class Model(nn.Module):
         self.e2 = encoder_block(64, 128)
         self.e3 = encoder_block(128, 256)
         self.e4 = encoder_block(256, 512)
+        self.e5 = encoder_block(512, 1024)
 
         """ Bottleneck """
-        self.b = conv_block(512, 1024)
+        self.b = conv_block(1024, 4096)
 
         """ Decoder """
-        self.d1 = decoder_block(1024, 512)
-        self.d2 = decoder_block(512, 256)
-        self.d3 = decoder_block(256, 128)
-        self.d4 = decoder_block(128, 64)
+        self.d1 = decoder_block(4096, 1024)
+        self.d2 = decoder_block(1024, 512)
+        self.d3 = decoder_block(512, 256)
+        self.d4 = decoder_block(256, 128)
+        self.d5 = decoder_block(128, 64)
 
         """ Classifier """
         self.outputs = nn.Conv2d(64, 34, kernel_size=1, padding=0)
@@ -95,18 +97,20 @@ class Model(nn.Module):
         s2, p2 = self.e2(p1)
         s3, p3 = self.e3(p2)
         s4, p4 = self.e4(p3)
+        s5, p5 = self.e5(p4)
 
         """ Bottleneck """
-        b = self.b(p4)
+        b = self.b(p5)
 
         """ Decoder """
-        d1 = self.d1(b, s4)
-        d2 = self.d2(d1, s3)
-        d3 = self.d3(d2, s2)
-        d4 = self.d4(d3, s1)
+        d1 = self.d1(b, s5)
+        d2 = self.d2(d1, s4)
+        d3 = self.d3(d2, s3)
+        d4 = self.d4(d3, s2)
+        d5 = self.d5(d4, s1)
 
         """ Classifier """
-        outputs = self.outputs(d4)
+        outputs = self.outputs(d5)
 
         return outputs
 
